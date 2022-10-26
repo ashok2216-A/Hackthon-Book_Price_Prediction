389c00e7f67e21d8c6cf34c89d276e58527490ef
ashok_2216 <ashoksiva2216.com>
2022-10-26 20:37:41 +0530

    code  changed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-     return "Hello World! My name is ashok"
+     return "Hello World! My name is ashok kumar"
 #      return render_template('index.html')
 
 if __name__ == "__main__":
