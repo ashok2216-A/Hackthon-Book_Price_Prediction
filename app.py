5282d13fe14d55f9455bf8a373377b1c5837f38a
ashok_2216 <ashoksiva2216.com>
2022-11-02 20:06:46 +0530

    file added

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+# from flask import Flask, render_template
+# app = Flask(__name__)
 
-@app.route("/")
-def home():
-     return "Hello World! My name is ashok"
-#      return render_template('index.html')
+# @app.route("/")
+# def home():
+#      return "Hello World! My name is ashok"
+#      # return render_template('templates\index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+import streamlit as st
+
+
+
+age1 = st.slider('How old are you?', 0, 5, 0)
+st.write("You Selected", age1, 'Star')
+
+age2 = st.slider('How old are you?', 0, 1000, 0)
+st.write("I'm ", age2, 'years old')
+
+age3 = st.slider('How old are you?', 0, 1302, 2500)
+st.write("I'm ", age3, 'years old')
+
+age4 = st.slider('How old are you?', 0, 2130, 215)
+st.write("I'm ", age4, 'years old')
+
+age5 = st.slider('How old are you?', 0, 6030, 2025)
+st.write("I'm ", age5, 'years old')
 
