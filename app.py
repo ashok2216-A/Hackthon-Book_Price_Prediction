593a4f67298c3094fd54d9e69b59e77d2749981d
ashok_2216 <ashoksiva2216.com>
2022-11-08 20:29:05 +0530

    code changed

--- app.py
+++ app.py
@@ -11,20 +11,69 @@
 
 import streamlit as st
 
+st.title("Book Price Prediction 🚀")
+#Reviews
+Reviews = st.slider('Select Reviews', 0, 5, 0)
+st.write("You Selected", Reviews, 'Star')
+
+#Edition Name
+EditionName = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', EditionName)
+
+#Ratings
+Ratings = st.slider('Select Ratings', 0, 1000, 0)
+st.write("You Selected", Ratings, 'Ratings')
+
+#Genre
+Genre = st.selectbox(
+    'How would you like to be contacted?',
+    ('Ema5il', 'Hom5e phone', 'Mob5ile phone'))
+
+st.write('You selected:', Genre)
 
 
-age1 = st.slider('How old are you?', 0, 5, 0)
-st.write("You Selected", age1, 'Star')
+#BookCategory
+Bookcategory = st.radio(
+    "What's your favorite movie genre",
+    (
+'Action & Adventure',                     
+'Arts, Film & Photography',                 
+'Biographies, Diaries & True Accounts',   
+'Comics & Mangas',                    
+'Computing, Internet & Digital Media',     
+'Crime, Thriller & Mystery',                
+'Humour',                                   
+'Language, Linguistics & Writing',          
+'Politics',                                 
+'Romance',                                  
+'Sports'))
 
-age2 = st.slider('How old are you?', 0, 1000, 0)
-st.write("I'm ", age2, 'years old')
+if Bookcategory == 'Action & Adventure':
+    st.write('You selected Action & Adventure.')
+elif Bookcategory == 'Arts, Film & Photography':
+    st.write('You selected Arts, Film & Photography.')
+elif Bookcategory == 'Biographies, Diaries & True Accounts':
+    st.write('You selected Biographies, Diaries & True Accounts.')
+elif Bookcategory == 'Comics & Mangas':
+    st.write('You selected Comics & Mangas.')
+elif Bookcategory == 'Computing, Internet & Digital Media':
+    st.write('You selected Computing, Internet & Digital Media.')
+elif Bookcategory == 'Crime, Thriller & Mystery':
+    st.write('You selected Crime, Thriller & Mystery.')
+elif Bookcategory == 'Humour':
+    st.write('You selected Humour.')
+elif Bookcategory == 'Language, Linguistics & Writing':
+    st.write('You selected Language, Linguistics & Writing.')
+elif Bookcategory == 'Politics':
+    st.write('You selected Politics.')
+elif Bookcategory == 'Romance':
+    st.write('You selected Romance.')
+elif Bookcategory == 'Sports':
+    st.write('You selected Sports.')
 
-age3 = st.slider('How old are you?', 0, 1302, 2500)
-st.write("I'm ", age3, 'years old')
-
-age4 = st.slider('How old are you?', 0, 2130, 215)
-st.write("I'm ", age4, 'years old')
-
-age5 = st.slider('How old are you?', 0, 6030, 2025)
-st.write("I'm ", age5, 'years old')
+else:
+    st.write("You didn't select any items.")
 
